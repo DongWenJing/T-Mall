e9b2af227c2749ded37e38c398cbccfb8cd0d3d8
小董不懂呀 <10352815+xiao-dong-doesnt-understand@user.noreply.gitee.com>
2022-03-19 16:00:50 +0800

    所有商品展示

--- src/main/java/com/tmall/service/impl/ShopServiceImpl.java
+++ src/main/java/com/tmall/service/impl/ShopServiceImpl.java
@@ -6,6 +6,8 @@ import com.tmall.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+
 /**
  * @Author : DongWJ
  * @Date : 2022/3/19 12:36
@@ -17,12 +19,9 @@ public class ShopServiceImpl implements ShopService {
     private ShopMapper shopMapper;
     @Override
     public void addUser(Shop shop) {
-        shop.setOwnerId(shop.getOwnerId());
-        shop.setShopName(shop.getShopName());
-        //shop.setIncome(shop.getIncome());
-        shop.setPassword(shop.getPassword());
-        shop.setRealName(shop.getRealName());
-        shop.setTelephone(shop.getTelephone());
+        //通过用户名获取user表中的userId值
+     BigInteger userId = shopMapper.getUserId(shop.getUsername());
+     shop.setOwnerId(userId);
         shopMapper.addUser(shop);
     }
 }
