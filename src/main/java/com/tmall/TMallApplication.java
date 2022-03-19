9050fb93742d6412788a76c5a7aa80cfc1d4deb8
ren-yu666 <1576082772@qq.com>
2022-03-19 10:34:52 +0800

    添加扫描注解

--- src/main/java/com/tmall/TMallApplication.java
+++ src/main/java/com/tmall/TMallApplication.java
@@ -1,9 +1,11 @@
 package com.tmall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tmall.mapper")
 public class TMallApplication {
 
     public static void main(String[] args) {
