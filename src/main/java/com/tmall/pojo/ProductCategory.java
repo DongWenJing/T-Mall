923b7cf280d8c757d4be3fe965fd7f1686c080b4
简单点 <10353151+heshuwu@user.noreply.gitee.com>
2022-03-21 14:48:52 +0800

    商品分类表

---
+++ src/main/java/com/tmall/pojo/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.tmall.pojo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/***
+ * 商品分类表
+ */
+
+@Data
+@Accessors(chain = true)
+public class ProductCategory implements Serializable {
+
+    //商品分类id
+    private BigInteger categoryId;
+    //商品分类名称
+    private String categoryName;
+}
